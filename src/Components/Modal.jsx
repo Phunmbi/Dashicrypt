e8296555580782abc595cb7a58d6972dd89d2ad1
Adeniyi Funmbi Adeyokunnu <funmbi.adeyokunnu@gmail.com>
2024-06-28 10:12:55 +0100

    ft: reformat modal content

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -4,6 +4,10 @@ import { TokenIcon } from "@token-icons/react"
 import Chart from "./Chart";
 
 const Modal = ({ handleClose, History, data }) => {
+  function capitalizeFirstLetter(str) {
+    return `${str[0].toUpperCase()}${str.slice(1)}`;
+  }
+
   return (
     <div className="modal is-active">
       <div className="modal-background" onClick={() => handleClose()} />
@@ -14,16 +18,16 @@ const Modal = ({ handleClose, History, data }) => {
           <div className="modal-title">
             <TokenIcon symbol={data.symbol} size={70} variant="branded" />
             <h2><strong>{data.name}</strong></h2>
-            <h2><strong>Symbol: {data.symbol}</strong></h2>
-            <h2><strong><a href={data.explorer}>{data.id}'s explorer</a></strong></h2>
+            <h2><strong>{data.symbol}</strong></h2>
+            <h2><strong><a href={data.explorer}>{`${capitalizeFirstLetter(data.id)}`}'s explorer</a></strong></h2>
           </div>
           <div className="modal-details-data has-text-left">
-            <h2><strong>Price:</strong> ${parseFloat(data.priceUsd).toFixed(2)}</h2>
-            <h2><strong>24hr price change:</strong> {parseFloat(data.changePercent24Hr).toFixed(2)}%</h2>
-            <h2><strong>Market Cap:</strong> ${parseFloat(data.marketCapUsd).toFixed(2)}</h2>
-            <h2><strong>Max Supply:</strong> {data.maxSupply ? parseFloat(data.maxSupply).toFixed(0) : 'N/A'}</h2>
-            <h2><strong>Average Supply:</strong> {parseFloat(data.supply).toFixed(0)}</h2>
-            <h2><strong>24hr trading volume:</strong> ${parseFloat(data.volumeUsd24Hr).toFixed(2)}</h2>
+            <h2><strong>Price: </strong>{new Intl.NumberFormat("en-US", {style: "currency",currency: "USD",}).format(data.priceUsd)}</h2>
+            <h2><strong>24hr price change: </strong> {new Intl.NumberFormat("en-US", {style: "percent",}).format(data.changePercent24Hr/100)}</h2>
+            <h2><strong>Market Cap: </strong> {new Intl.NumberFormat("en-US", {style: "currency",currency: "USD",}).format(data.marketCapUsd)}</h2>
+            <h2><strong>Max Supply: </strong> {data.maxSupply ? new Intl.NumberFormat("en-US", {style: "decimal"}).format(data.maxSupply): 'N/A'}</h2>
+            <h2><strong>Average Supply: </strong> {new Intl.NumberFormat("en-US", {style: "decimal"}).format(data.supply)}</h2>
+            <h2><strong>24hr trading volume: </strong> {new Intl.NumberFormat("en-US", {style: "currency",currency: "USD",}).format(data.volumeUsd24Hr)}</h2>
           </div>
         </div>
         <Chart History={History} />
